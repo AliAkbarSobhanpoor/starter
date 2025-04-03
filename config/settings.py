35b0e38eb2182959dcfdce674f1561d26e75d9cf
aliakbarsobhanpour <aliakbarsobhanpoor@gmail.com>
2025-04-03 22:29:01 +0330

    config for django filler and eazy_thumbnail to work.

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # internal apps
     'users',
+    # external packages
+    'easy_thumbnails',
+    'filer',
 ]
 
 MIDDLEWARE = [
@@ -131,5 +135,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 AUTH_USER_MODEL =   'users.User'
+
+# easy thumbnail config for image cropping and so on
+
+THUMBNAIL_PROCESSORS = (
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    #'easy_thumbnails.processors.scale_and_crop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filters',
+)
+FILER_CANONICAL_URL = 'sharing/'
